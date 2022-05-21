7be2bb3a93f616b581e3453be63becce505b44df
MarawanEmara <93253811+MarawanEmara@users.noreply.github.com>
2022-05-21 08:23:28 +0200

    Created a React app connected to Flask

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/time")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (<div>
+
+    {(typeof data === "undefined") ? (<div>Loading...</div>) : (<div>{data.time}</div>)}
+
+  </div>);
 }
 
 export default App;
